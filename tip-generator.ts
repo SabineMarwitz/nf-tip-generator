8fa976d4bdd7293bec6c8d0ef588b1366d2651d5
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-07-21 15:53:37 +0200

    draft

---
+++ tip-generator.ts
@@ -0,0 +1,45 @@
+// class TipCalculator
+/*
+check: number
+tip_percentage: number = 10
+num_people?: number = 1
+
+function calculateTotal()
+
+function calculateTip()
+
+function calculateTipPerPerson()
+
+function printTip()
+
+Output:
+--- Tip Calculation Summary ---
+Check Amount: $50.00
+Tip Percentage: 15%
+Tip Amount: $7.50
+Total Bill: $57.50
+Divide among people: yes
+Split between how many people: 2
+Each person pays: $28.75
+-----------------------------
+
+
+*/
+
+// class Bill
+
+
+
+// class UserInputHandler
+/*
+Question: "How high is the check?"
+Answer: "50.00"
+
+Question: "What percentage of tip will you give? (e.g., 15 for 15%)"
+Answer: "15"
+
+Question: "Should the bill be split among multiple people? (yes/no)"
+Answer: "yes"
+Question: "How many people will split the bill?"
+Answer: "2"
+*/
